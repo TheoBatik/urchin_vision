027c59658ea87976b2e5c943025adbb765350333
TheoBatik <60322861+TheoBatik@users.noreply.github.com>
2022-04-01 18:24:37 +0200

    Functions module added

--- get_widths.py
+++ get_widths.py
@@ -8,45 +8,14 @@ import numpy as np
 import imutils
 import cv2
 
+import functions as f
 
 # SETUP
 
-def empty(a):
-    pass
-
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
-
-def midpoint(ptA, ptB):
-	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+# Functions
+stack_images = f.stack_images
+empty = f.empty
+midpoint = f.midpoint
 
 
 # # construct the argument parse and parse the arguments
@@ -116,7 +85,7 @@ while True:
     img_col = cv2.bitwise_and(img,img,mask=mask)
     
     # scale and display filtered image
-    img_col_scaled = stackImages(0.2, ([ img_col ] ))
+    img_col_scaled = stack_images(0.2, ([ img_col ] ))
     cv2.imshow("Filter by HSV", img_col_scaled)
 
 cv2.destroyAllWindows()
@@ -303,9 +272,9 @@ while True:
             print('Contours: \n\t total = ', len(cnts[0]), 'Large enough = ', count_in, ' too small = ', count_out)   
             print('\t=> done.')
         
-        img_result_scaled = stackImages(0.2, ([ img_result ] ))
+        img_result_scaled = stack_images(0.2, ([ img_result ] ))
         cv2.imshow("The measurement", img_result_scaled)
-        img_stack = stackImages(0.1, ([ [img_col, edged], [dilated, eroded] ] ))
+        img_stack = stack_images(0.1, ([ [img_col, edged], [dilated, eroded] ] ))
         cv2.imshow("Image stack: TL = filtered by colour; TR = edged (Canny); BL = dilated; BR = eroded ", img_stack)
         
     except Exception:
